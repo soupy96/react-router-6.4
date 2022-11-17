481d30e6d920ee643060078ff6ade031d8bdc6f7
Michael Campbell <michael.campbell14@gmail.com>
2022-11-17 14:53:27 -0500

    CHORE using and returning data with an action

--- src/pages/NewPost.jsx
+++ src/pages/NewPost.jsx
@@ -1,30 +1,11 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { redirect, useActionData, useNavigate } from 'react-router-dom';
 
 import NewPostForm from '../components/NewPostForm';
 import { savePost } from '../util/api';
 
 function NewPostPage() {
-  const [isSubmitting, setIsSubmitting] = useState(false);
-  const [error, setError] = useState();
+  const data = useActionData();
   const navigate = useNavigate();
-
-  async function submitHandler(event) {
-    event.preventDefault();
-    setIsSubmitting(true);
-    try {
-      const formData = new FormData(event.target);
-      const post = {
-        title: formData.get('title'),
-        body: formData.get('post-text'),
-      };
-      await savePost(post);
-      navigate('/');
-    } catch (err) {
-      setError(err);
-    }
-    setIsSubmitting(false);
-  }
 
   function cancelHandler() {
     navigate('/blog');
@@ -32,14 +13,28 @@ function NewPostPage() {
 
   return (
     <>
-      {error && <p>{error.message}</p>}
-      <NewPostForm
-        onCancel={cancelHandler}
-        onSubmit={submitHandler}
-        submitting={isSubmitting}
-      />
+      {data && data.status && <p>{data.message}</p>}
+      <NewPostForm onCancel={cancelHandler} submitting={false} />
     </>
   );
 }
 
 export default NewPostPage;
+
+export async function action({ request }) {
+  const formData = await request.formData();
+  const post = {
+    title: formData.get('title'),
+    body: formData.get('post-text'),
+  };
+  try {
+    await savePost(post);
+  } catch (err) {
+    if (err.status === 422) {
+      // returning instead of throwing the error we stay on the current page
+      return err;
+    }
+    throw err;
+  }
+  return redirect('/blog');
+}
